a5907a2bb7afc25bbda5999f9b39b4d97f880944
nathaliadv <nathaliadantasv@gmail.com>
2021-04-10 17:12:30 -0300

    delete file and new path

--- js/main.js
+++
@@ -1,4 +0,0 @@
-import createCards from "./createCards.js";
-import getProducts from "./getProducts.js";
-import loadProducts from "./loadProducts.js";
-import renderProducts from "./renderProducts.js";
