636fec6273f6140c1dfd38c4198eb5abbe3580e9
nathaliadv <nathaliadantasv@gmail.com>
2021-04-09 21:58:33 -0300

    script js with all files js

---
+++ js/main.js
@@ -0,0 +1,4 @@
+import createCards from "./createCards.js";
+import getProducts from "./getProducts.js";
+import loadProducts from "./loadProducts.js";
+import renderProducts from "./renderProducts.js";
